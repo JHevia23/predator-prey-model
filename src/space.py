7d105837257fa91e9a8d4ed5a2af47669ca3b00a
Juan Segundo Hevia <juan.hevia@tekal.ai>
2021-03-03 16:15:17 -0300

    added sheep deaths and visualization

--- src/space.py
+++ src/space.py
@@ -1,6 +1,7 @@
 import numpy as np
 import custom_utils as utils
 import matplotlib.pyplot as plt
+from animals import Wolf, Sheep
 
 class Grid():
 
@@ -10,6 +11,7 @@ class Grid():
         self.YMAX = len_y
         # self.LAST_KEY = 0
         self.agent_population = {}
+        self.dead_sheep_locations = []
 
     def add_agent(self, agent, agent_id):
         ## add agent to the space's population
@@ -26,3 +28,32 @@ class Grid():
         
         return subset_population
 
+    def update_population(self):
+        '''
+        After each iteration, the grid will look up for 'DEAD' sheep and remove them from the population
+        '''
+        # loop through sheeps
+        sheeps = [agent for agent in self.agent_population.values() if agent.TYPE == utils.TYPE_CONSTANT_SHEEP]
+        for sheep in sheeps:
+            if self.check_predators(sheep):
+                # remove the sheep from the population
+                self.agent_population.pop(sheep.ID)
+                print(f"SHEEP {sheep.ID} DIED")
+                self.dead_sheep_locations.append(sheep.pos)
+                print(f"DEAD SHEEP COUNT {len(self.dead_sheep_locations)}")
+
+
+
+    def check_predators(self, sheep):
+        '''
+        Returns True if there are predators at a fatal distance
+        '''
+        wolves = [agent for agent in self.agent_population.values() if agent.TYPE == utils.TYPE_CONSTANT_WOLF]
+        distances_id = [np.linalg.norm(wolf.pos - sheep.pos) for wolf in wolves]
+        closest_distance = np.min(distances_id)
+
+        if closest_distance <= utils.SIGHT_RADIUS_WOLF:
+            return True
+        else:
+            return False
+        
